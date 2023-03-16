4abb5b1b2032ce9ccaecded583e97ad328f4e9fd
zhoulei <zhoulei@wangyuan.com>
2023-03-16 15:36:25 +0800

    给作为测试model的Department,Employee添加openapi中文描述，但有个问题是导出pdf后中文会乱码，没有找到好的解决办法。

--- department-service/src/main/java/pl/piomin/services/department/model/Department.java
+++ department-service/src/main/java/pl/piomin/services/department/model/Department.java
@@ -1,5 +1,8 @@
 package pl.piomin.services.department.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.SchemaProperties;
+import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,17 +15,22 @@ import java.util.List;
 @Setter
 @ToString
 @NoArgsConstructor
+@Schema(description = "某个部门的描述信息")
 public class Department {
 
-	private Long id;
-	private Long organizationId;
-	private String name;
-	private List<Employee> employees = new ArrayList<>();
+    @Schema(description = "部门ID")
+    private Long id;
+    @Schema(description = "组织ID")
+    private Long organizationId;
+    @Schema(description = "部门名称")
+    private String name;
+    @Schema(description = "部门内员工名单")
+    private List<Employee> employees = new ArrayList<>();
 
-	public Department(Long organizationId, String name) {
-		super();
-		this.organizationId = organizationId;
-		this.name = name;
-	}
+    public Department(Long organizationId, String name) {
+        super();
+        this.organizationId = organizationId;
+        this.name = name;
+    }
 
 }
--- department-service/src/main/java/pl/piomin/services/department/model/Employee.java
+++ department-service/src/main/java/pl/piomin/services/department/model/Employee.java
@@ -1,5 +1,6 @@
 package pl.piomin.services.department.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,17 +10,22 @@ import lombok.ToString;
 @Getter
 @ToString
 @NoArgsConstructor
+@Schema(description = "员工信息")
 public class Employee {
 
-	private Long id;
-	private String name;
-	private int age;
-	private String position;
-	
-	public Employee(String name, int age, String position) {
-		this.name = name;
-		this.age = age;
-		this.position = position;
-	}
+    @Schema(description = "工号")
+    private Long id;
+    @Schema(description = "姓名")
+    private String name;
+    @Schema(description = "年龄")
+    private int age;
+    @Schema(description = "职位")
+    private String position;
+
+    public Employee(String name, int age, String position) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
 
 }
